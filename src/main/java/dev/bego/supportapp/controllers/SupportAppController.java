d57af08e9fa937150ddb681bdb91c2dd427c43bf
Bego Blanco <begoblanfer@gmail.com>
2024-07-10 10:02:31 +0200

    added service and controller

--- src/main/java/dev/bego/supportapp/controllers/SupportAppController.java
+++ src/main/java/dev/bego/supportapp/controllers/SupportAppController.java
@@ -1,5 +1,38 @@
 package dev.bego.supportapp.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import dev.bego.supportapp.models.SupportApp;
+import dev.bego.supportapp.services.SupportAppService;
+
+@RestController
+@RequestMapping("/api/support-requests")
 public class SupportAppController {
+
+       @Autowired
+    private SupportAppService service;
+
+    @GetMapping
+    public List<SupportApp> getAllRequests() {
+        return service.findAll();
+    }
+
+    @PostMapping
+    public SupportApp createRequest(@RequestBody SupportApp supportApp) {
+        return service.save(supportApp);
+    }
+
+    @PutMapping("/{id}")
+    public SupportApp updateRequest(@PathVariable Long id, @RequestBody SupportApp supportApp) {
+        return service.update(id, supportApp);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteRequest(@PathVariable Long id) {
+        service.delete(id);
+    }
     
 }
--- src/main/java/dev/bego/supportapp/services/SupportAppService.java
+++ src/main/java/dev/bego/supportapp/services/SupportAppService.java
@@ -1,5 +1,37 @@
 package dev.bego.supportapp.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dev.bego.supportapp.models.SupportApp;
+import dev.bego.supportapp.repositories.SupportAppRepository;
+
+@Service
 public class SupportAppService {
+     @Autowired
+    private SupportAppRepository repository;
+
+    public List<SupportApp> findAll() {
+        return repository.findAll();
+    }
+
+    public SupportApp save(SupportApp supportApp) {
+        return repository.save(supportApp);
+    }
+
+    public SupportApp update(Long id, SupportApp supportApp) {
+        return repository.findById(id).map(existingSupportApp -> {
+            existingSupportApp.setRequesterName(supportApp.getRequesterName());
+            existingSupportApp.setTopic(supportApp.getTopic());
+            existingSupportApp.setDescription(supportApp.getDescription());
+            return repository.save(existingSupportApp);
+        }).orElseThrow(() -> new RuntimeException("Support request not found with id " + id));
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
     
 }

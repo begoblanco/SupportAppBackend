7d4bd200a52c5ee9c0adc521568a7c24bea8246d
Bego Blanco <begoblanfer@gmail.com>
2024-07-12 21:20:53 +0200

    fixes

--- src/main/java/dev/bego/supportapp/services/RequestService.java
+++ src/main/java/dev/bego/supportapp/services/RequestService.java
@@ -1,20 +1,19 @@
 package dev.bego.supportapp.services;
 
-import java.util.List;
-
-// import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import dev.bego.supportapp.models.Request;
 import dev.bego.supportapp.repositories.RequestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class RequestService {
-    //  @Autowired
-    // private RequestRepository repository;
 
     private final RequestRepository repository;
 
+    @Autowired
     public RequestService(RequestRepository repository) {
         this.repository = repository;
     }
@@ -23,21 +22,23 @@ public class RequestService {
         return repository.findAll();
     }
 
-    public Request store(Request supportApp) {
-        return repository.save(supportApp);
+    public Request store(Request request) {
+        request.setRequestDate(LocalDateTime.now());
+        return repository.save(request);
     }
 
-    public Request update(Long id, Request supportApp) {
-        return repository.findById(id).map(existingSupportApp -> {
-            existingSupportApp.setRequesterName(supportApp.getRequesterName());
-            existingSupportApp.setTopic(supportApp.getTopic());
-            existingSupportApp.setDescription(supportApp.getDescription());
-            return repository.save(existingSupportApp);
-        }).orElseThrow(() -> new RuntimeException("Support request not found with id " + id));
+    public Request update(Long id, Request updatedRequest) {
+        return repository.findById(id)
+                .map(request -> {
+                    request.setRequesterName(updatedRequest.getRequesterName());
+                    request.setTopic(updatedRequest.getTopic());
+                    request.setDescription(updatedRequest.getDescription());
+                    return repository.save(request);
+                })
+                .orElseThrow(() -> new RuntimeException("Request not found with id " + id));
     }
 
     public void delete(Long id) {
         repository.deleteById(id);
     }
-    
 }
